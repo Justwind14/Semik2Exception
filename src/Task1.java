ed4393f51553e61246aebfb0e40e8577db9c1a0f
Justwind <Justwind14@yandex.ru>
2023-06-06 20:31:42 +0300

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,47 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Изучите данный код. Все ли с ним хорошо? Если нет,
+ * скорректируйте код, попробуйте обосновать свое решение.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        Scanner scanner = new Scanner(System.in);
+//        int[] arr = new int[10];
+//        System.out.println(
+//                "Укажите индекс элемента массива, " +
+//                        "в который хотите записать значение 1");
+//        int index = 0;
+//        if (scanner.hasNextInt()){
+//            index = scanner.nextInt();
+//        }
+//        else
+//        {System.err.println("введено не число");
+//        throw new InputMismatchException();}
+//
+//        try {
+//            arr[index] = 1;
+//        } catch (IndexOutOfBoundsException e){
+//            System.err.println("Указан индекс за пределами массива");
+//        }
+//
+//        try {
+//            int index = scanner.nextInt();
+//            arr[index] = 1;
+//        }
+//        catch (IndexOutOfBoundsException e) {
+//        }
+//        catch (InputMismatchException e){
+//            System.err.println("индекс элемента массива введен некорректно");
+//        }
+//        catch (RuntimeException e){System.err.println("Указан индекс за пределами массива");}
+
+
+
+    }
+}
---
+++ src/Task2.java
@@ -0,0 +1,52 @@
+/**
+ * Обработайте возможные исключительные ситуации. "Битые" значения
+ * в исходном массиве считайте нулями. Можно внести в код правки,
+ * которые считаете необходимыми
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        String[][] arr0 = new String[][] {
+                {"1", "1", "1", "1", "1"},
+                {"1", "1", "1", "1", "1"},
+                {"1", "1", "1", "1", "1"},
+                {"1", "1", "1", "1", "1"},
+                {"1", "1", "1", "1", "1"}
+        };
+
+        String[][] arr = new String[][] {
+                {"1", "1"},
+                {"1", "1", "1", "1", "1"},
+                {"1", "2"},
+                {"3", "3", "d"},
+                {"2", "6"}
+        };
+
+        String[][] arr1 = null;
+        System.out.println(sum2d(arr1));
+
+    }
+
+    private static int sum2d(String[][] arr) {
+        int sum = 0;
+
+            try {
+                for (int i = 0; i < arr.length; i++) {
+                    for (int j = 0; j < arr[i].length; j++) {
+                        int val = Integer.parseInt(arr[i][j]);
+                        sum += val;
+                    }
+                }
+            }
+
+            catch (NumberFormatException e) {
+                System.err.println("элементы массивов должны состоять из целых чисел");
+            }
+            catch (NullPointerException e) {
+                System.err.println("массив не может быть найден");
+            }
+            System.out.println("я всё равно выполнюсь");
+            return sum;
+
+
+    }
+}
